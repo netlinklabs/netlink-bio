d77cef809ef0b9744601c28103173097ae5db367
Netlink Developer <netlinktoken@gmail.com>
2026-08-06 13:19:20 +0700

    Add social media links in footer
    Added social media links to the footer section.

--- shared/site-nav.js
+++ shared/site-nav.js
@@ -85,6 +85,15 @@
                 <a href="${link('#changelog')}">Changelog</a>
               </div>
             </div>
+            <div class="footer-social">
+              <h4 class="footer-col-title">Social</h4>
+              <div class="footer-links">
+                <a href="https://twitter.com/netlinkbio" target="_blank" rel="noopener noreferrer">X (Twitter)</a>
+                <a href="https://linkedin.com/company/netlinklabs" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a href="https://github.com/netlinklabs" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <a href="https://youtube.com/@netlink" target="_blank" rel="noopener noreferrer">YouTube</a>
+              </div>
+            </div>
           </div>
           <div class="footer-group footer-group-right">
             <div>
@@ -107,15 +116,6 @@
                 <a href="acceptable-use.html">Acceptable Use Policy</a>
                 <a href="security.html">Security</a>
               </div>
-            </div>
-          </div>
-          <div>
-            <h4 class="footer-col-title">Social</h4>
-            <div class="footer-links">
-              <a href="https://twitter.com/netlinkbio" target="_blank" rel="noopener noreferrer">X (Twitter)</a>
-              <a href="https://linkedin.com/company/netlinklabs" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-              <a href="https://github.com/netlinklabs" target="_blank" rel="noopener noreferrer">GitHub</a>
-              <a href="https://youtube.com/@netlink" target="_blank" rel="noopener noreferrer">YouTube</a>
             </div>
           </div>
         </div>
